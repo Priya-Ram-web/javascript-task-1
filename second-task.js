0ae782af0e70bfa9168f8f4fe91508c13de1bca6
Priya-Ram-web <basudey456@gmail.com>
2024-08-29 12:10:11 +0600

    second-task.js

---
+++ second-task.js
@@ -0,0 +1,13 @@
+var marksOfMathematics = 75.25;
+var marksOfBiology = 65;
+var marksOfChemistry = 80;
+var marksOfPhysics = 35.45;
+var marksOfBengali = 99.50;
+var marksOfStudent = marksOfMathematics + marksOfBiology + marksOfChemistry + marksOfPhysics + marksOfBengali;
+console.log(marksOfStudent);
+
+var marksOfStudent = 355.2;
+var average = marksOfStudent/5;
+console.log(average);
+var result = average.toFixed(2);
+console.log(result);
