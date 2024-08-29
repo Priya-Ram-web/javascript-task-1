e5400161ed626020b4e02118f7668d3daec09e45
Priya-Ram-web <basudey456@gmail.com>
2024-08-29 11:30:17 +0600

    first javascript task

---
+++ first-task.js
@@ -0,0 +1,10 @@
+var iHaveTk = 1000;
+var costOfOranges = 350;
+var costOfApples = 350;
+var totalCost = costOfOranges + costOfApples;
+console.log(totalCost);
+
+var iHaveTk = 1000;
+var totalCost = 700;
+var returnTK = iHaveTk - totalCost;
+console.log(returnTK);
